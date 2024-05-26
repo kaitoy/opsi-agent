8c7441a364ad351b6909f74af3220cd966336dc1
kaitoy <kaitoy@pcap4j.org>
2024-05-26 15:09:46 +0900

    RAG with a system prompt.

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 import sys
 
 import nest_asyncio
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders.sitemap import SitemapLoader
 from langchain_community.vectorstores.inmemory import InMemoryVectorStore
-from langchain_openai import OpenAIEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 input = sys.argv[1]
 
@@ -28,5 +30,26 @@ index = VectorstoreIndexCreator(
 
 retriever = index.vectorstore.as_retriever()
 
-response = retriever.invoke(input)
+prompt = ChatPromptTemplate.from_messages([
+    (
+        "system",
+        """contextに基づいて、Ops Iの質問になるべく頑張って答えてください。ただし、Ops Iと関係ない質問に対しては、知るかボケと回答してもいいです:
+
+<context>
+{context}
+</context>
+"""
+    ),
+    (
+        "human",
+        "質問: {input}"
+    )
+])
+llm = ChatOpenAI(model="gpt-3.5-turbo")
+chain = create_stuff_documents_chain(llm, prompt)
+
+response = chain.invoke({
+    "input": input,
+    "context": retriever.invoke(input),
+})
 print(response)
