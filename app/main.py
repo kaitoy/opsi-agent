9f3bf509a1db24c63eb0367c082cda0f80b322a3
kaitoy <kaitoy@pcap4j.org>
2024-05-26 09:43:44 +0900

    First LangChain RAG app.

--- app/main.py
+++ app/main.py
@@ -1,10 +1,30 @@
 import sys
 
-from langchain_openai import ChatOpenAI
+import nest_asyncio
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders.sitemap import SitemapLoader
+from langchain_community.vectorstores.inmemory import InMemoryVectorStore
+from langchain_openai import OpenAI, OpenAIEmbeddings
 
 input = sys.argv[1]
 
-llm = ChatOpenAI(model="gpt-3.5-turbo")
+nest_asyncio.apply()
 
-response = llm.invoke(input)
+text_splitter = CharacterTextSplitter(
+    separator = "\n",
+    chunk_size = 400,
+    chunk_overlap = 0,
+    length_function = len,
+)
+
+loader = SitemapLoader(web_path="https://itpfdoc.hitachi.co.jp/manuals/JCS/JCSM71020002/sitemap.xml")
+
+index = VectorstoreIndexCreator(
+    vectorstore_cls=InMemoryVectorStore,
+    embedding=OpenAIEmbeddings(),
+    text_splitter=text_splitter,
+).from_loaders([loader])
+
+response = index.query(input, OpenAI(temperature=0))
 print(response)
